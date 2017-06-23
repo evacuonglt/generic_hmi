3044a9c3b389069b1ee1505736498a5469666934
リーツアン　クオン <lcuong@yahoo-corp.jp>
2017-06-23 11:07:05 +0700

    - update code

--- src/js/Controllers/Controller.js
+++ src/js/Controllers/Controller.js
@@ -22,7 +22,7 @@ export default class Controller {
         // this.vehicleInfoController = new VehicleInfoController;
     }
     connectToSDL() {
-        this.socket = new WebSocket("ws://localhost:8087")
+        this.socket = new WebSocket(url)
         this.socket.onopen = this.onopen.bind(this)
         this.socket.onclose = this.onclose.bind(this)
         this.socket.onmessage = this.onmessage.bind(this)
